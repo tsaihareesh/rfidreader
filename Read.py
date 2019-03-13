e1d2e173b62f80b18e0b0df17e20a2c5e29138b2
Saihareesh Thathamsetty <saihareesh.thathamsetty@target.com>
2019-03-13 14:54:06 +0530

    fix

--- Read.py
+++ Read.py
@@ -55,8 +55,8 @@ while continue_reading:
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
-    if status == MIFAREReader.MI_OK:
-        #print "Card detected"
+    # if status == MIFAREReader.MI_OK:
+    #     print "Card detected"
     
     # Get the UID of the card
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
